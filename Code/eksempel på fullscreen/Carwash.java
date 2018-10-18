161814f405e20d1c63c999a44d9f8aa70dbe1c6b
Martin Kjeldgaard Nikolajsen <mart56p7@stud.kea.dk>
2018-10-18 08:46:40 +0200

    These are my changes

--- Code/eksempel på fullscreen/Carwash.java
+++ Code/eksempel på fullscreen/Carwash.java
@@ -23,9 +23,9 @@ class WashFrame extends SwingWorker<Object, Object>
 	Font fontmedium;
 	Font fontsmall;
 	JFrame frame;
-	
+
 	JPanel[] screens = null;
-	
+
 	public WashFrame()
 	{
 		//Setting up our font setting
@@ -35,101 +35,102 @@ class WashFrame extends SwingWorker<Object, Object>
 		//Prepares our frame, which we want to fill the entire screen
 		frame = new JFrame();
 		frame.setUndecorated(true);
-		GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();     
+		GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
 		gd.setFullScreenWindow(frame);
 		frame.setLayout(null);
-		frame.addWindowListener(new WindowAdapter() {
-			public void windowClosed(WindowEvent e) {
-				cancel(true);
-				System.out.println("windowClosed");
-				Runtime.getRuntime().exit(0);
-			}
-			
-			public void windowClosing(WindowEvent e) {
-				cancel(true);
-				System.out.println("windowClosing");
-				Runtime.getRuntime().exit(0);
-			}			
-		});
+		frame.addWindowListener(
+				new WindowAdapter() {
+					public void windowClosed(WindowEvent e) {
+						cancel(true);
+						System.out.println("windowClosed");
+						Runtime.getRuntime().exit(0);
+					}
+
+					public void windowClosing(WindowEvent e) {
+						cancel(true);
+						System.out.println("windowClosing");
+						Runtime.getRuntime().exit(0);
+					}
+				});
 		//Saves our screen width / height
 		screenwidth = frame.getWidth();
 		screenheight = frame.getHeight();
 		//Creates our info screen
 		this.execute();
-		
+
 
 	}
-   
+
 	public Object doInBackground() throws Exception
 	{
 		screens = new JPanel[3];
-		
+
 		screens[0] = new InsertCardScreen(this);
 		screens[0].setBounds(0, 0, screenwidth, screenheight);
 		screens[0].setVisible(false);
 		frame.add(screens[0]);
-		
+
 		screens[1] = new InfoScreen(this);
 		screens[1].setBounds(0, 0, screenwidth, screenheight);
 		screens[1].setVisible(false);
 		frame.add(screens[1]);
-		
+
 		screens[2] = new ErrorScreen(this);
 		screens[2].setBounds(0, 0, screenwidth, screenheight);
 		screens[2].setVisible(false);
-		frame.add(screens[2]);		
-		
-		
+		frame.add(screens[2]);
+
+
 		int n = 0;
-		System.out.println("doInBackground started");		
+		System.out.println("doInBackground started");
 		while(!isCancelled())
 		{
-			System.out.println("doInBackground 1");	
+			System.out.println("doInBackground 1");
 			if(n % 2 == 0)
 			{
-			System.out.println("doInBackground 2");				
-				screens[1].setVisible(true);				
+				System.out.println("doInBackground 2");
+				screens[1].setVisible(true);
 				screens[2].setVisible(false);
 			}
 			else
 			{
-			System.out.println("doInBackground 3");				
-				screens[1].setVisible(false);				
+				System.out.println("doInBackground 3");
+				screens[1].setVisible(false);
 				screens[2].setVisible(true);
 			}
 			n++;
 			System.out.println("doInBackground 4");
-			Thread.sleep(1000);			
+			Thread.sleep(1000);
 		}
 		System.out.println("doInBackground returning");
 		return(null);
 	}
-	
+
 	public int getWidth()
 	{
 		return(screenwidth);
 	}
-	
+
 	public int getHeight()
 	{
 		return(screenheight);
 	}
-	
+
 	public Font getFontBig()
 	{
 		return(fontbig);
 	}
-	
+
 	public Font getFontMedium()
 	{
 		return(fontmedium);
 	}
-	
+
 	public Font getFontSmall()
 	{
 		return(fontsmall);
 	}
-	
+
 	public static Dimension getTextDimensions(Component c, Font font, String text)
 	{
 		FontMetrics metrics = c.getFontMetrics(font);
@@ -140,23 +141,23 @@ class WashFrame extends SwingWorker<Object, Object>
 		// and render context
 		int adv = metrics.stringWidth(text);
 		// calculate the size of a box to hold the
-		// text with some padding.	
+		// text with some padding.
 		return(new Dimension(adv+2, hgt+2));
 	}
-	
+
 	/**
-	Resets system dependencies.
-	- Reset the current user object
-	*/
+	 Resets system dependencies.
+	 - Reset the current user object
+	 */
 	public void SystemReset()
 	{
-		
+
 	}
-	
+
 	/**
-	Checks that the washing hall is working.
-	Checks that there is an active connection to the mainframe
-	*/
+	 Checks that the washing hall is working.
+	 Checks that there is an active connection to the mainframe
+	 */
 	public boolean SystemCheck()
 	{
 		return(true);
@@ -180,10 +181,10 @@ class InsertCardScreen extends JPanel
 		//Calculate the size of the text in the label
 		Dimension labelsize = wf.getTextDimensions(b, wf.getFontMedium(), blabel);
 		//Centers the label
-		b.setBounds(_wf.getWidth()/2-(int)(labelsize.getWidth()/2),_wf.getHeight()/2-(int)(labelsize.getHeight()/2),(int)labelsize.getWidth(),(int)labelsize.getHeight());  
+		b.setBounds(_wf.getWidth()/2-(int)(labelsize.getWidth()/2),_wf.getHeight()/2-(int)(labelsize.getHeight()/2),(int)labelsize.getWidth(),(int)labelsize.getHeight());
 		this.add(b);
-		
-		this.setBackground(Color.GREEN);		
+
+		this.setBackground(Color.GREEN);
 	}
 }
 
@@ -203,10 +204,10 @@ class ErrorScreen extends JPanel
 		//Calculate the size of the text in the label
 		Dimension labelsize = wf.getTextDimensions(b, wf.getFontMedium(), blabel);
 		//Centers the label
-		b.setBounds(_wf.getWidth()/2-(int)(labelsize.getWidth()/2),_wf.getHeight()/2-(int)(labelsize.getHeight()/2),(int)labelsize.getWidth(),(int)labelsize.getHeight());  
+		b.setBounds(_wf.getWidth()/2-(int)(labelsize.getWidth()/2),_wf.getHeight()/2-(int)(labelsize.getHeight()/2),(int)labelsize.getWidth(),(int)labelsize.getHeight());
 		this.add(b);
-		
-		this.setBackground(Color.GREEN);		
+
+		this.setBackground(Color.GREEN);
 	}
 }
 
@@ -217,16 +218,17 @@ class InfoScreen extends JPanel
 	{
 		wf = _wf;
 		this.setLayout(null);
-		JButton b=new JButton("Click Here");  
-		b.setBounds(50,100,95,30);  
-b.addActionListener(new ActionListener()
-    {
-      public void actionPerformed(ActionEvent e)
-      {
-        b.setText("Stop clicking me!");
-      }
-    });		
-		this.add(b);	  
+		JButton b=new JButton("Click Here");
+		b.setBounds(50,100,95,30);
+		b.addActionListener(
+				new ActionListener()
+				{
+					public void actionPerformed(ActionEvent e)
+					{
+						b.setText("Stop clicking me!");
+					}
+				});
+		this.add(b);
 		this.setBackground(Color.BLACK);
 		JButton button = new JButton("bla");
 		this.add(button);
