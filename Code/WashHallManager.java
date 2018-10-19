e5eedcf1375363d18c34b779793223dd76bb620a
Martin Kjeldgaard Nikolajsen <mart56p7@stud.kea.dk>
2018-10-19 13:09:34 +0200

    Account change

--- Code/WashHallManager.java
+++ Code/WashHallManager.java
@@ -1,15 +1,15 @@
 enum Operation{
-    STANDARD_WASH,
-    EARLY_STANDARD_WASH,
-    ECONOMY_WASH,
-    EARLY_ECONOMY_WASH,
-    DELUX_WASH,
-    ABORT,
-    RECHARGE,
-    INSERT_CARD,
-    RECEIPT,
-    NO_RECEIPT,
-    START_WASH
+   STANDARD_WASH,
+   EARLY_STANDARD_WASH,
+   ECONOMY_WASH,
+   EARLY_ECONOMY_WASH,
+   DELUX_WASH,
+   ABORT,
+   RECHARGE,
+   INSERT_CARD,
+   RECEIPT,
+   NO_RECEIPT,
+   START_WASH
 }
 
 class WashHallManager
@@ -23,53 +23,53 @@ class WashHallManager
    
    public int runCommand(Operation o)
    {
-       int returnDisplay = 0;
-       switch(o) {
-       case STANDARD_WASH:
-	   System.out.println("standard wash");
-	   returnDisplay = 3;
-	   break;
-       case ECONOMY_WASH:
-	   System.out.println("ECONOMY wash");
-	   returnDisplay = 3;
-	   break;
-       case DELUX_WASH:
-	   System.out.println("DELUX wash");
-	   returnDisplay = 3;
-	   break;
-       case EARLY_STANDARD_WASH:
-	   System.out.println("EARLYSTANDARD wash");
-	   returnDisplay = 3;
-	   break;
-       case EARLY_ECONOMY_WASH:
-	   System.out.println("EARLYECONOMY wash");
-	   returnDisplay = 3;
-	   break;
-       case ABORT:
-	   System.out.println("ABORT CALLED");
-	   returnDisplay = 0;
-	   break;
-       case RECHARGE:
-	   System.out.println("RECHARGE");
-	   returnDisplay = 0;
-	   break;
-       case INSERT_CARD:
-	   System.out.println("INSERT CARD");
-	   returnDisplay = 1;
-	   break;
-       case START_WASH:
-	   System.out.println("START WASH");
-	   returnDisplay = 6;
-	   break;
-       case RECEIPT:
-	   System.out.println("RECEIPT");
-	   returnDisplay = 5;
-	   break;
-       case NO_RECEIPT:
-	   System.out.println("NO RECEIPT");
-	   returnDisplay = 5;
-	   break;
-       }
+      int returnDisplay = 0;
+      switch(o) {
+         case STANDARD_WASH:
+            System.out.println("standard wash");
+            returnDisplay = 3;
+            break;
+         case ECONOMY_WASH:
+            System.out.println("ECONOMY wash");
+            returnDisplay = 3;
+            break;
+         case DELUX_WASH:
+            System.out.println("DELUX wash");
+            returnDisplay = 3;
+            break;
+         case EARLY_STANDARD_WASH:
+            System.out.println("EARLYSTANDARD wash");
+            returnDisplay = 3;
+            break;
+         case EARLY_ECONOMY_WASH:
+            System.out.println("EARLYECONOMY wash");
+            returnDisplay = 3;
+            break;
+         case ABORT:
+            System.out.println("ABORT CALLED");
+            returnDisplay = 0;
+            break;
+         case RECHARGE:
+            System.out.println("RECHARGE");
+            returnDisplay = 0;
+            break;
+         case INSERT_CARD:
+            System.out.println("INSERT CARD");
+            returnDisplay = 1;
+            break;
+         case START_WASH:
+            System.out.println("START WASH");
+            returnDisplay = 6;
+            break;
+         case RECEIPT:
+            System.out.println("RECEIPT");
+            returnDisplay = 5;
+            break;
+         case NO_RECEIPT:
+            System.out.println("NO RECEIPT");
+            returnDisplay = 5;
+            break;
+      }
       return(returnDisplay);
    }
     
@@ -80,7 +80,7 @@ class WashHallManager
    public Account getAccount()
    {
       System.out.println("returning account");   
-      return(new Account());
+      return(new Account(0, 0));
    }
    
    public String getWashDoneTime()
