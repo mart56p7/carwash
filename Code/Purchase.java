186ac5b40ec542d0b52d8b6cd1ce9a6e82ed4165
rhoeberg <contact@rasmushoeberg.net>
2018-10-23 14:05:37 +0200

    added purchase class

---
+++ Code/Purchase.java
@@ -0,0 +1,54 @@
+import java.time.format.DateTimeFormatter;  
+import java.time.LocalDateTime;
+
+public class Purchase
+{
+
+    int id;
+    int accountId;
+    String washType;
+    double washPrice;
+    LocalDateTime washTimeStamp;
+    
+    public Purchase(int accountId, String washType, double washPrice) {
+	this.accountId = accountId;
+	this.washType = washType;
+	this.washPrice = washPrice;
+
+	DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
+	washTimeStamp = LocalDateTime.now();  
+    }
+
+    public Purchase(int id, int accountId, String washType, double washPrice, LocalDateTime washTimeStamp) {
+	this.id = id;
+	this.accountId = accountId;
+	this.washType = washType;
+	this.washPrice = washPrice;
+	this.washTimeStamp = washTimeStamp;
+    }
+
+    public int getId()
+    {
+	return accountId;
+    }
+    
+    public int getAccountId()
+    {
+	return accountId;
+    }
+    
+    public String getWashType()
+    {
+	return washType;
+    }
+    
+    public double getWashPrice()
+    {
+	return washPrice;
+    }
+    
+    public LocalDateTime getWashTimeStamp()
+    {
+	return washTimeStamp;
+    }
+}
